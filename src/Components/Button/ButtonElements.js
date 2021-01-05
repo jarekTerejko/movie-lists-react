9fe82ade06c5ab04c28ed3860e077a92872e0b83
jarekTerejko <j.terejko@gmail.com>
2021-01-05 05:30:52 +0100

    add pointer to Button

--- src/Components/Button/ButtonElements.js
+++ src/Components/Button/ButtonElements.js
@@ -4,6 +4,8 @@ export const ButtonWrapper = styled.button`
   border: none;
   background: none;
   padding: 1rem;
+  cursor: pointer;
+  margin-right: 0.5rem;
 `;
 
 export const ButtonImg = styled.img``;
